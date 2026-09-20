04622863332805939bad935e4fbe51be9571a012
hamzaalhsnawi320-lang <hamzaalhsnawi320@gmail.com>
2026-09-20 13:13:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,7 @@
-const BLOCKED_TYPES = ['image', 'video', 'audio'];
-
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => self.clients.claim());
-
 self.addEventListener('fetch', event => {
-  // احظر كل الصور والفيديوهات والصوتيات وكل ملف mp4 jpg png
-  if (
-    BLOCKED_TYPES.includes(event.request.destination) ||
-    event.request.url.match(/\.(jpg|jpeg|png|gif|webp|mp4|mov|avi|webm|mp3|m3u8)$/i)
-  ) {
-    return event.respondWith(
-      new Response('', { status: 403, headers: { 'Content-Type': 'text/plain' } })
-    );
+  if (['image','video','audio'].includes(event.request.destination) || event.request.url.match(/\.(jpg|png|mp4|gif|webm)$/i)) {
+    return event.respondWith(new Response('', {status: 403}));
   }
 });
