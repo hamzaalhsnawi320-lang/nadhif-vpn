fc48230630323a9b0126c8c4e9db88b5d9bc0fd0
hamzaalhsnawi320-lang <hamzaalhsnawi320@gmail.com>
2026-09-20 13:25:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', event => {
-  if (['image','video','audio'].includes(event.request.destination) || event.request.url.match(/\.(jpg|png|mp4|gif|webm)$/i)) {
-    return event.respondWith(new Response('', {status: 403}));
-  }
-});
+// حاليا بدون حجب حتى يرجع الرمز
