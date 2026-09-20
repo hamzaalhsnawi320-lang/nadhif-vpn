b709818c818272c0a2eb5994c07a3c7577108bb0
hamzaalhsnawi320-lang <hamzaalhsnawi320@gmail.com>
2026-09-20 10:47:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,10 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('nadhif-v1').then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE = "nadhif-v1";
+const FILES = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
